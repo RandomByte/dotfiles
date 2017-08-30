3d35a74cde6b02e18463a7efcd8521b575407539
Merlin Beutlberger <m.beutlberger@sap.com>
2017-08-30 17:10:09 +0200

    Initial commit
    Based on https://github.com/matz3/dotfiles/commit/69654d3c1400b11187c0ad7b8cae6bb427c9af1d

---
+++ zsh/aliases.zsh
@@ -0,0 +1,9 @@
+alias reload!='. ~/.zshrc'
+alias ni='npm install'
+alias nig='npm install --global'
+alias nid='npm install --save-dev'
+alias nis='npm install --save'
+
+alias such='git'
+alias very='git'
+alias wow='git status -sb'
