856b6396377ab803f46133787d1fb4aea86d26ac
Merlin Beutlberger <r@ndombyte.net>
2020-04-11 16:27:53 +0200

    Ubuntu: Add keybindings for ctrl-right/-left
    As per https://serverfault.com/a/386938

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -44,3 +44,7 @@ bindkey '^[[5C' end-of-line
 bindkey '^[[3~' delete-char
 bindkey '^[^N' newtab
 bindkey '^?' backward-delete-char
+
+# Required additional bindings on Ubuntu
+bindkey '\e[1;5C' forward-word        # ctrl right
+bindkey '\e[1;5D' backward-word       # ctrl left
