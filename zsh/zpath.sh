57221ba72845233a6e2f5239a1f38eeee4a248e6
Merlin Beutlberger <r@ndombyte.net>
2020-12-11 11:50:09 +0100

    macOS: Install Yarn manually
    Otherwise an additionally installed Node+npm version in the Homebrew lib
    dir  will be used when spawning a shell in a script executed via Yarn.

--- zsh/zpath.sh
+++ zsh/zpath.sh
@@ -17,6 +17,9 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
 
     # Ruby
     source $ZSH/ruby/env.zsh
+
+    # Yarn path
+    PATH="$ZSH/bin/yarn/bin:$PATH"
 else
 	test -d /home/linuxbrew/.linuxbrew && eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
 	# or just add /home/linuxbrew/.linuxbrew/bin to $PATH?
